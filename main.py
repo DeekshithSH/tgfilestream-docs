608a85e1ec824b60fa3532c7a90c1903dd8c1be2
Deekshith SH <shdeekshith@gmail.com>
2025-04-03 17:18:15 +0530

    updated requirements

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from os import environ
 if (not environ.get("PUBLIC_URL")) and environ.get("RENDER_EXTERNAL_URL"):
     environ["PUBLIC_URL"]=environ.get("RENDER_EXTERNAL_URL")
 
-importlib.import_module("TGFileStream.__main__".lower())
+importlib.import_module("tgfs.__main__")
